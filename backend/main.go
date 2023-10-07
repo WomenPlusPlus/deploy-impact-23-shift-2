8e1149fad3e78d46eb427a79cf847a670982bd2f
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-10-07 22:48:45 +0200

    add custom log output

--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/controller"
+	"gitlab.com/pragmaticreviews/golang-gin-poc/middlewares"
 	"gitlab.com/pragmaticreviews/golang-gin-poc/service"
 )
 
@@ -11,8 +15,16 @@ var (
 	userController controller.UserController = controller.New(userService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+	setupLogOutput()
+
+	server := gin.New()
+	server.Use(gin.Recovery(), middlewares.Logger())
 
 	server.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
