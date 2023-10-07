36de5f383e3ddc734cea00a093ae4053504b7291
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-10-07 23:01:37 +0200

    add docstring to user controller

--- backend/controller/user-controller.go
+++ backend/controller/user-controller.go
@@ -1,3 +1,5 @@
+// Package controller provides the implementation of the UserController interface
+// for handling user-related operations in the Gin web framework.
 package controller
 
 import (
@@ -6,25 +8,33 @@ import (
 	"gitlab.com/pragmaticreviews/golang-gin-poc/service"
 )
 
+// UserController defines the methods that must be implemented by a user controller.
 type UserController interface {
+	// FindAll retrieves a list of all users.
 	FindAll() []entity.User
+
+	// Save saves a user entity based on the request data in the given context.
 	Save(ctx *gin.Context) entity.User
 }
 
+// userController is the implementation of the UserController interface.
 type userController struct {
 	service service.UserService
 }
 
+// New creates a new instance of UserController with the provided UserService.
 func New(service service.UserService) UserController {
 	return &userController{
 		service: service,
 	}
 }
 
+// FindAll retrieves a list of all users.
 func (controller *userController) FindAll() []entity.User {
 	return controller.service.FindAll()
 }
 
+// Save saves a user entity based on the request data in the given context.
 func (controller *userController) Save(ctx *gin.Context) entity.User {
 	var user entity.User
 	ctx.BindJSON((&user))
