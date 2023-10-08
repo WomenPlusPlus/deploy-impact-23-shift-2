7a8ff78dc22a22a3bfa951fe9cdb59e6c86a9db3
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-10-08 23:47:51 +0200

    update api

--- backend/api.go
+++ backend/api.go
@@ -9,11 +9,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-// WriteJSON writes a JSON response with the given status code and value.
-func WriteJSON(w http.ResponseWriter, status int, value interface{}) error {
-	w.WriteHeader(status)
-	w.Header().Add("Content-Type", "application/json")
-	return json.NewEncoder(w).Encode(value)
+// APIServer represents an HTTP server for the JSON API.
+type APIServer struct {
+	address string
+	userDB  UserDB
+}
+
+// NewAPIServer creates a new instance of APIServer with the given address.
+func NewAPIServer(address string, userDB UserDB) *APIServer {
+	return &APIServer{
+		address: address,
+		userDB:  userDB,
+	}
 }
 
 // apiFunc represents a function that handles API requests.
@@ -24,24 +31,19 @@ type apiError struct {
 	Error string
 }
 
+// WriteJSON writes a JSON response with the given status code and value.
+func WriteJSON(w http.ResponseWriter, status int, value interface{}) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(value)
+}
+
 // makeHTTPHandleFunc creates an HTTP request handler function for the provided apiFunc.
 func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
 			WriteJSON(w, http.StatusBadRequest, apiError{Error: err.Error()})
 		}
-	}
-}
-
-// APIServer represents an HTTP server for the JSON API.
-type APIServer struct {
-	address string
-}
-
-// NewAPIServer creates a new instance of APIServer with the given address.
-func NewAPIServer(address string) *APIServer {
-	return &APIServer{
-		address: address,
 	}
 }
 
@@ -73,13 +75,22 @@ func (s *APIServer) handleUser(w http.ResponseWriter, r *http.Request) error {
 
 // handleGetUser handles GET requests for user account information.
 func (s *APIServer) handleGetUser(w http.ResponseWriter, r *http.Request) error {
-	account := NewUser("Test", "User", "Mrs.", "test_user@testusers.test", "Online", "https://placehold.co/400", "Candidate")
-	return WriteJSON(w, http.StatusOK, account)
+	user := NewUser("Test", "User", "Mrs.", "test_user@testusers.test", "Online", "https://placehold.co/400", "Candidate")
+	return WriteJSON(w, http.StatusOK, user)
 }
 
 // handleCreateUser handles POST requests to create a new user account.
 func (s *APIServer) handleCreateUser(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	userRequest := new(CreateUserRequest)
+	if err := json.NewDecoder(r.Body).Decode(userRequest); err != nil {
+		return err
+	}
+
+	user := NewUser("Test", "User", "Mrs.", "test_user@testusers.test", "Online", "https://placehold.co/400", "Candidate")
+	if err := s.userDB.CreateUser(user); err != nil {
+		return err
+	}
+	return WriteJSON(w, http.StatusOK, user)
 }
 
 // handleDeleteUser handles DELETE requests to delete a user account.
