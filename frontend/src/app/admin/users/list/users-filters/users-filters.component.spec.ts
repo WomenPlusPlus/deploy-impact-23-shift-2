1cd8b69833b73e545f3fd9276a6bfc2f1de53a40
João Rodrigues <jotar910@gmail.com>
2023-10-08 21:29:17 +0100

    fix unit tests

--- frontend/src/app/admin/users/list/users-filters/users-filters.component.spec.ts
+++ frontend/src/app/admin/users/list/users-filters/users-filters.component.spec.ts
@@ -1,4 +1,12 @@
+import { EMPTY } from 'rxjs';
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { provideComponentStore } from '@ngrx/component-store';
+import { provideMockStore } from '@ngrx/store/testing';
+
+import { AdminUsersService } from '@app/admin/users/common/services/admin-users.service';
+import { UsersListStore } from '@app/admin/users/list/users-list.store';
 
 import { UsersFiltersComponent } from './users-filters.component';
 
@@ -8,7 +16,12 @@ describe('UsersFiltersComponent', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [UsersFiltersComponent]
+            imports: [UsersFiltersComponent],
+            providers: [
+                { provide: AdminUsersService, useValue: { getList: () => EMPTY } },
+                provideMockStore(),
+                provideComponentStore(UsersListStore)
+            ]
         });
         fixture = TestBed.createComponent(UsersFiltersComponent);
         component = fixture.componentInstance;
--- frontend/src/app/admin/users/list/users-list.component.spec.ts
+++ frontend/src/app/admin/users/list/users-list.component.spec.ts
@@ -2,7 +2,11 @@ import { EMPTY } from 'rxjs';
 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideComponentStore } from '@ngrx/component-store';
+import { provideMockStore } from '@ngrx/store/testing';
+
 import { AdminUsersService } from '@app/admin/users/common/services/admin-users.service';
+import { UsersListStore } from '@app/admin/users/list/users-list.store';
 
 import { UsersListComponent } from './users-list.component';
 
@@ -13,7 +17,11 @@ describe('UsersListComponent', () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [UsersListComponent],
-            providers: [{ provide: AdminUsersService, useValue: { getList: () => EMPTY } }]
+            providers: [
+                { provide: AdminUsersService, useValue: { getList: () => EMPTY } },
+                provideMockStore(),
+                provideComponentStore(UsersListStore)
+            ]
         });
         fixture = TestBed.createComponent(UsersListComponent);
         component = fixture.componentInstance;
