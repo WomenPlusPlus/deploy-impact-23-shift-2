786f6794dd06a37eab5128f39343d15fa66031e2
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-10-08 21:01:55 +0200

    update handle account func with dummy data from Account type

--- backend/api.go
+++ backend/api.go
@@ -64,7 +64,9 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	account := NewAccount("Test", "User", "Mrs.", "test_user@testusers.test", "Online", "https://placehold.co/400", "Candidate")
+	return WriteJSON(w, http.StatusOK, account)
+
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
