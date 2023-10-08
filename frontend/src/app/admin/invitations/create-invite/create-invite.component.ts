2ecf39f0e1ebad27533031d58b8018fe61bd53c8
João Rodrigues <jotar910@gmail.com>
2023-10-08 22:26:15 +0100

    add dynamic options and address styling

--- frontend/src/app/admin/invitations/create-invite/create-invite.component.ts
+++ frontend/src/app/admin/invitations/create-invite/create-invite.component.ts
@@ -5,15 +5,27 @@ import { Router } from '@angular/router';
 
 import { CreateInviteFormModel } from '@app/admin/invitations/create-invite/common/models/create-invite.model';
 import { UserKindEnum, UserRoleEnum } from '@app/common/models/users.model';
+import { UserCompanyRoleLabelPipe } from '@app/common/pipes/user-company-role-label/user-company-role-label.pipe';
+import { UserKindLabelPipe } from '@app/common/pipes/user-kind-label/user-kind-label.pipe';
 
 @Component({
     selector: 'app-create-invite',
     standalone: true,
-    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, UserKindLabelPipe, UserCompanyRoleLabelPipe],
     templateUrl: './create-invite.component.html'
 })
 export class CreateInviteComponent implements OnInit {
     form!: CreateInviteFormModel;
+
+    readonly userKinds: UserKindEnum[] = [
+        UserKindEnum.ADMIN,
+        UserKindEnum.CANDIDATE,
+        UserKindEnum.COMPANY,
+        UserKindEnum.ASSOCIATION
+    ];
+    readonly userRoles: UserRoleEnum[] = [UserRoleEnum.ADMIN, UserRoleEnum.USER];
+
+    protected readonly userKindEnum = UserKindEnum;
 
     constructor(private router: Router) {}
 
@@ -28,7 +40,7 @@ export class CreateInviteComponent implements OnInit {
     private initForm(): void {
         this.form = {
             kind: UserKindEnum.CANDIDATE,
-            role: UserRoleEnum.USER,
+            role: UserRoleEnum.ADMIN,
             email: '',
             subject: '',
             message: ''
