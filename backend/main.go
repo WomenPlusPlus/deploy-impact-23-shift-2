7100c20158e4e6eb7999b59ee7f5d98ff4184d2d
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-10-08 23:48:53 +0200

    implement userDB into server

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
-	server := NewAPIServer(":5555")
+	userDB, err := NewPostgresDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := userDB.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	server := NewAPIServer(":3000", userDB)
 	server.Run()
 }
