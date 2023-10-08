f21fbf9b025eee53d5550ced5083d6a949334651
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-10-08 23:46:51 +0200

    update user struct

--- backend/types.go
+++ backend/types.go
@@ -1,14 +1,22 @@
 package main
 
+import "time"
+
 type User struct {
-	ID            int
-	FirstName     string
-	LastName      string
-	PreferredName string
-	Email         string
-	State         string
-	ImageUrl      string
-	Role          string
+	ID            int       `json:"id"`
+	FirstName     string    `json:"first_name"`
+	LastName      string    `json:"last_name"`
+	PreferredName string    `json:"preferred_name"`
+	Email         string    `json:"email"`
+	State         string    `json:"state"`
+	ImageUrl      string    `json:"image_url"`
+	Role          string    `json:"role"`
+	Created       time.Time `json:"created"`
+}
+
+type CreateUserRequest struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
 
 func NewUser(firstName, lastName, preferredName, email, state, imageUrl, role string) *User {
@@ -21,5 +29,6 @@ func NewUser(firstName, lastName, preferredName, email, state, imageUrl, role st
 		State:         state,
 		ImageUrl:      imageUrl,
 		Role:          role,
+		Created:       time.Now().UTC(),
 	}
 }
