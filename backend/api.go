515809be9f155067e07adba815cd81cdef5e3ef6
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-10-08 21:10:31 +0200

    add docstring in api.go

--- backend/api.go
+++ backend/api.go
@@ -9,18 +9,22 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func WriteJSON(w http.ResponseWriter, status int, value any) error {
+// WriteJSON writes a JSON response with the given status code and value.
+func WriteJSON(w http.ResponseWriter, status int, value interface{}) error {
 	w.WriteHeader(status)
 	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(value)
 }
 
+// apiFunc represents a function that handles API requests.
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
+// apiError represents an API error response.
 type apiError struct {
 	Error string
 }
 
+// makeHTTPHandleFunc creates an HTTP request handler function for the provided apiFunc.
 func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
@@ -29,16 +33,19 @@ func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 	}
 }
 
+// APIServer represents an HTTP server for the JSON API.
 type APIServer struct {
 	address string
 }
 
+// NewAPIServer creates a new instance of APIServer with the given address.
 func NewAPIServer(address string) *APIServer {
 	return &APIServer{
 		address: address,
 	}
 }
 
+// Run starts the HTTP server and listens for incoming requests.
 func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
@@ -49,6 +56,7 @@ func (s *APIServer) Run() {
 	http.ListenAndServe(s.address, router)
 }
 
+// handleAccount handles requests related to user accounts.
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
 	if r.Method == "GET" {
 		return s.handleGetAccount(w, r)
@@ -63,20 +71,23 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 	return fmt.Errorf("method not allowed %s", r.Method)
 }
 
+// handleGetAccount handles GET requests for user account information.
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
 	account := NewAccount("Test", "User", "Mrs.", "test_user@testusers.test", "Online", "https://placehold.co/400", "Candidate")
 	return WriteJSON(w, http.StatusOK, account)
-
 }
 
+// handleCreateAccount handles POST requests to create a new user account.
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
+// handleDeleteAccount handles DELETE requests to delete a user account.
 func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
+// handleUpdateAccount handles PUT requests to update a user account.
 func (s *APIServer) handleUpdateAccount(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
