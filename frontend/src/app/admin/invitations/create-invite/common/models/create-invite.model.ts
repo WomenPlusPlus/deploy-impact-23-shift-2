d2df0b31491708f371e62a3756eda00d619b60ea
João Rodrigues <jotar910@gmail.com>
2023-10-09 21:05:49 +0100

    use reactive forms to introduce validations

--- frontend/src/app/admin/invitations/create-invite/common/models/create-invite.model.ts
+++ frontend/src/app/admin/invitations/create-invite/common/models/create-invite.model.ts
@@ -1,4 +1,14 @@
+import { FormControl } from '@angular/forms';
+
 import { UserKindEnum, UserRoleEnum } from '@app/common/models/users.model';
+
+export interface CreateInviteFormGroup {
+    kind: FormControl<UserKindEnum | null>;
+    role: FormControl<UserRoleEnum | null>;
+    email: FormControl<string | null>;
+    subject: FormControl<string | null>;
+    message: FormControl<string | null>;
+}
 
 export interface CreateInviteFormModel {
     kind: UserKindEnum;
--- frontend/src/app/admin/invitations/create-invite/create-invite.component.ts
+++ frontend/src/app/admin/invitations/create-invite/create-invite.component.ts
@@ -1,21 +1,33 @@
+import { startWith, Subscription } from 'rxjs';
+
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { CreateInviteFormModel } from '@app/admin/invitations/create-invite/common/models/create-invite.model';
+import { CreateInviteFormGroup } from '@app/admin/invitations/create-invite/common/models/create-invite.model';
+import { LetDirective } from '@app/common/directives/let/let.directive';
 import { UserKindEnum, UserRoleEnum } from '@app/common/models/users.model';
+import { FormErrorMessagePipe } from '@app/common/pipes/form-error-message/form-error-message.pipe';
 import { UserCompanyRoleLabelPipe } from '@app/common/pipes/user-company-role-label/user-company-role-label.pipe';
 import { UserKindLabelPipe } from '@app/common/pipes/user-kind-label/user-kind-label.pipe';
 
 @Component({
     selector: 'app-create-invite',
     standalone: true,
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, UserKindLabelPipe, UserCompanyRoleLabelPipe],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        FormErrorMessagePipe,
+        UserKindLabelPipe,
+        UserCompanyRoleLabelPipe,
+        LetDirective
+    ],
     templateUrl: './create-invite.component.html'
 })
-export class CreateInviteComponent implements OnInit {
-    form!: CreateInviteFormModel;
+export class CreateInviteComponent implements OnInit, OnDestroy {
+    form!: FormGroup<CreateInviteFormGroup>;
 
     readonly userKinds: UserKindEnum[] = [
         UserKindEnum.ADMIN,
@@ -25,12 +37,20 @@ export class CreateInviteComponent implements OnInit {
     ];
     readonly userRoles: UserRoleEnum[] = [UserRoleEnum.ADMIN, UserRoleEnum.USER];
 
-    protected readonly userKindEnum = UserKindEnum;
+    protected readonly subscriptions: Subscription[] = [];
 
-    constructor(private router: Router) {}
+    constructor(
+        private readonly router: Router,
+        private readonly fb: FormBuilder
+    ) {}
 
     ngOnInit(): void {
         this.initForm();
+        this.initSubscriptions();
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.forEach((s) => s.unsubscribe());
     }
 
     onSubmit(): void {
@@ -38,12 +58,24 @@ export class CreateInviteComponent implements OnInit {
     }
 
     private initForm(): void {
-        this.form = {
-            kind: UserKindEnum.CANDIDATE,
-            role: UserRoleEnum.ADMIN,
-            email: '',
-            subject: '',
-            message: ''
-        };
+        this.form = this.fb.group({
+            kind: this.fb.control(UserKindEnum.CANDIDATE, [Validators.required]),
+            role: this.fb.control(UserRoleEnum.ADMIN, [Validators.required]),
+            email: this.fb.control('', [Validators.required, Validators.minLength(5), Validators.maxLength(512)]),
+            subject: this.fb.control('', [Validators.required, Validators.minLength(3), Validators.maxLength(256)]),
+            message: this.fb.control('', [Validators.required, Validators.minLength(25), Validators.maxLength(1024)])
+        });
+    }
+
+    private initSubscriptions(): void {
+        this.subscriptions.push(
+            this.form.controls.kind.valueChanges
+                .pipe(startWith(this.form.controls.kind.value))
+                .subscribe((kind) =>
+                    kind !== UserKindEnum.ASSOCIATION && kind !== UserKindEnum.COMPANY
+                        ? this.form.controls.role.disable()
+                        : this.form.controls.role.enable()
+                )
+        );
     }
 }
