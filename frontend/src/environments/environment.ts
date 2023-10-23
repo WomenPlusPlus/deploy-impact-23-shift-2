d58fa50b0232f7227fb5d15d37112149347677a7
João Rodrigues <jotar910@gmail.com>
2023-10-23 21:38:08 +0100

    connect users feature to be

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export default {
-    API_BASE_URL: 'https://shift2-deployimpact.xyz',
+    // API_BASE_URL: 'https://shift2-deployimpact.xyz',
+    API_BASE_URL: 'http://localhost:8080',
     AUTH0_DOMAIN: "shift2-deployimpact.eu.auth0.com",
     AUTH0_CLIENT_ID: "PCcsclXRoCKimPmlrjo61uNi6rjgZApn"
 };
