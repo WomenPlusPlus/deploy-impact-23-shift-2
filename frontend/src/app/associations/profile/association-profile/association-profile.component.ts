131d0bfe9befa50f57eab248c42484effe102c54
Mandy <34437702+mandyjker@users.noreply.github.com>
2023-10-25 21:42:53 +0300

    fix association profile

--- frontend/src/app/associations/profile/association-profile/association-profile.component.ts
+++ frontend/src/app/associations/profile/association-profile/association-profile.component.ts
@@ -1,9 +1,14 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGlobe, faExternalLink } from '@fortawesome/free-solid-svg-icons';
+
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { provideComponentStore } from '@ngrx/component-store';
 
+import { UserKindEnum } from '@app/common/models/users.model';
+import { IsAuthorizedPipe } from '@app/common/pipes/is-authorized/is-authorized.pipe';
 import { ContentErrorComponent } from '@app/ui/content-error/content-error.component';
 import { ContentLoadingComponent } from '@app/ui/content-loading/content-loading.component';
 
@@ -12,13 +17,17 @@ import { AssociationProfileStore } from './association-profile.store';
 @Component({
     selector: 'app-association-profile',
     standalone: true,
-    imports: [CommonModule, ContentErrorComponent, ContentLoadingComponent],
+    imports: [CommonModule, ContentErrorComponent, ContentLoadingComponent, FontAwesomeModule, IsAuthorizedPipe],
     providers: [provideComponentStore(AssociationProfileStore)],
     templateUrl: './association-profile.component.html'
 })
 export class AssociationProfileComponent {
     id?: number;
     readonly vm$ = this.associationProfileStore.vm$;
+
+    protected readonly faGlobe = faGlobe;
+    protected readonly faExternalLink = faExternalLink;
+    protected readonly userKindEnum = UserKindEnum;
 
     constructor(
         private readonly associationProfileStore: AssociationProfileStore,
